11173838527561e608a8ef36a67915fb639c0146
Henry Savary-Jackson <96239862+Henry-Savary-Jackson@users.noreply.github.com>
2023-12-14 22:51:31 +0200

    volume added

--- blocks.h
+++ blocks.h
@@ -3,12 +3,16 @@
 static const Block blocks[] = {
     /*Icon*/ /*Command*/ /*Update Interval*/ /*Update Signal*/
     {"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g", 30, 0},
-    {"Temp:",
-     "curl wttr.in/?T | sed '4q;d' |gawk 'match($0, /([+-]?[0-9()]+) °C/, "
-     "res){ print res[1] space \" °C\"}'",
-     120, 0},
-    {"Network", "nmcli connection show | grep wifi | awk '{print $1}'", 10, 0},
+    //    {"Temp:",
+    //    "curl wttr.in/?T | sed '4q;d' |gawk 'match($0, /([+-]?[0-9()]+) °C/, "
+    //    "res){ print res[1] space \" °C\"}'",
+    //   120, 0},
+    {"Battery:", "batteryinfo", 5, 0},
+    {"Network:", "nmcli connection show | sed '2q;d' | awk '{print $1}'", 10,
+     0},
     {"", "date '+%b %d (%a) %I:%M:%S%p'", 1, 0},
+    {" ", "amixer sget Master | awk -F'[][]' '/Left:/ { print $2 }'", 1, 0}
+
 };
 
 // sets delimeter between status commands. NULL character ('\0') means no
